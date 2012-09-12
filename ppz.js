95cdd15025a062f1705eb23a72df55b4e16c34d2
Vajkard Valvazor <vajkard.valvazor@gmail.com>
2012-09-12 15:28:08 +0200

    upload files
    test r

---
+++ ppz.js
@@ -0,0 +1,19 @@
+var ppz = function(rows, columns) {
+	  
+	  var matrix = new Array(rows); // initialize an array with proper number of [rows] and [columns]
+	 
+	  for (var i = 0; i < rows; i++) { // creating two dimensional matrix
+		  matrix[i] = new Array(columns);
+		  }
+
+	  for(var i = 0; i < rows; i++) {
+	      for(var j = 0; j < columns; j++) {
+	    	if (Math.random() > 0.5) // if the random number is greater than 0.5
+	    		{matrix[i][j] = 1}  // the value written in the matrix is 1
+	        else
+				{matrix[i][j] = 0}  // else, we write 0
+	    	}
+	      }
+
+	  return matrix;
+	  };
